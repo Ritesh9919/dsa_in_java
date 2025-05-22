c337128a6fd61df305de0278f35782970daa5e78
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-05-22 11:30:21 +0530

    Add Questions.java for string search functionality

---
+++ linearSearch/Questions.java
@@ -0,0 +1,23 @@
+package linearSearch;
+
+public class Questions {
+
+    public static boolean searchInString(String str, char target) {
+        if (str.length() == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String str = "ritesh";
+        char target = 's';
+        System.out.println(searchInString(str, target));
+    }
+}
