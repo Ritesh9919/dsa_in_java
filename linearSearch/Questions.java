d17a1fbb0a2e1058314eeb6484168bf4b968c48d
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-05-22 11:35:38 +0530

    Add searchInRange method to Questions.java for targeted element search in a specified range

--- linearSearch/Questions.java
+++ linearSearch/Questions.java
@@ -15,9 +15,24 @@ public class Questions {
         return false;
     }
 
+    public static int searchInRange(int[] arr, int target, int start, int end) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        for (int i = start; i <= end; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         String str = "ritesh";
         char target = 's';
         System.out.println(searchInString(str, target));
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        System.out.println(searchInRange(arr, 5, 2, 5));
     }
 }
