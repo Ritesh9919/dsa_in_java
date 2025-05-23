e5e98fb25693b127ddac70cfdb5f37d4a1c488e3
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-05-23 12:20:50 +0530

    implemented binary search

---
+++ binarySearch/Code.java
@@ -0,0 +1,30 @@
+package binarySearch;
+
+public class Code {
+
+    public static int solution(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            // int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        int target = 6;
+        System.out.println(solution(arr, target));
+    }
+}
