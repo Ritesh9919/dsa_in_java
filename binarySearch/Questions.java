6fc2fe156ca64b43069ee1920cb7cab70c968eec
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-06-11 11:28:40 +0530

    Add searchInInfiniteArr and binarySearch methods to Questions.java for searching in infinite arrays

--- binarySearch/Questions.java
+++ binarySearch/Questions.java
@@ -41,10 +41,37 @@ public class Questions {
         return end;
     }
 
+    public static int searchInInfiniteArr(int[] arr, int target) {
+        int start = 0;
+        int end = 1;
+
+        while (target > arr[end]) {
+            int temp = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = temp;
+        }
+        return binarySearch(arr, target, start, end);
+    }
+
+    public static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] numbers = { 2, 4, 6, 8, 11, 13 };
-        int target = 10;
-        System.out.println(ceillingOfNumbers(numbers, target));
-        System.out.println(floorOfNumbers(numbers, target));
+        int target = 2;
+        // System.out.println(ceillingOfNumbers(numbers, target));
+        // System.out.println(floorOfNumbers(numbers, target));
+        System.out.println(searchInInfiniteArr(numbers, target));
     }
 }
