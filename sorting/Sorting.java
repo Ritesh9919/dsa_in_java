42e4e660ce9747147f1a4fc15f3c9de90c64ce23
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-06-15 11:35:08 +0530

    Add insertionSort method to Sorting class for sorting an array using insertion sort

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -42,10 +42,25 @@ public class Sorting {
         }
     }
 
+    public static void insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = { 3, 1, 4, 5, 2 };
         // bubbleSort(arr);
-        selectionSort(arr);
+        // selectionSort(arr);
+        insertionSort(arr);
         System.out.println(Arrays.toString(arr));
 
     }
