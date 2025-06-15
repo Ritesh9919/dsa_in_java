a128377789f0a07cf05b1f76b377099d76e4a8ee
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-06-15 10:45:15 +0530

    Add selectionSort method to Sorting class for sorting an array using selection sort

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -22,9 +22,30 @@ public class Sorting {
 
     }
 
+    public static int getMax(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start + 1; i <= end; i++) {
+            if (arr[i] > arr[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int lastIndex = arr.length - i - 1;
+            int maxEleIndex = getMax(arr, 0, lastIndex);
+            int temp = arr[maxEleIndex];
+            arr[maxEleIndex] = arr[lastIndex];
+            arr[lastIndex] = temp;
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = { 3, 1, 4, 5, 2 };
-        bubbleSort(arr);
+        // bubbleSort(arr);
+        selectionSort(arr);
         System.out.println(Arrays.toString(arr));
 
     }
