6d07c0f942b4da6785598e220a82888ca2574d7c
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-06-25 15:59:28 +0530

    Add Code class with methods for generating various patterns

---
+++ pattern/Code.java
@@ -0,0 +1,45 @@
+package pattern;
+
+public class Code {
+
+    public static void pattern1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        pattern4(n);
+    }
+}
